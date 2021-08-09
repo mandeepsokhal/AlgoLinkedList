5b36478d79e3b1a6fc6ec88dc296d954bcaae88e
Mandeep Sokhal <mandeepsokhal@outlook.com>
2021-08-09 21:07:13 +0530

    Add project files.

---
+++ AlgoLinkedList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AlgoLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node first = new Node() { Value = 5 };
+            Node second = new Node() { Value = 6 };
+            first.Next = second;
+
+            Node third = new Node() { Value = 3 };
+            second.Next = third;
+            PrintOutSingleLinkedList(first);
+            Console.Read();
+        }
+
+        private static void PrintOutSingleLinkedList(Node node)
+        {
+            while(node!= null)
+            {
+                Console.WriteLine(node.Value);
+                node = node.Next;
+            }
+        }
+    }
+}
---
+++ AlgoLinkedList/SingleLinkedList.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace AlgoLinkedList
+{
+    public class SingleLinkedList<T>
+    {
+        public Node<T> Head { get; private set; }
+        public Node<T> Tail { get; private set; }
+
+        public int count { get; private set; }
+
+        public void AddFirst(T value)
+        {
+            AddFirst(new Node<T>(value));
+        }
+
+        private void AddFirst(Node<T> node)
+        {
+            Node<T> tmp = Head;
+
+            Head = node;
+
+            Head.Next = tmp;
+            count++;
+
+            if (count == 1)
+            {
+                Tail = Head;
+            }
+
+        }
+
+        public void AddLast(T Value)
+        {
+            AddLast(new Node<T>(Value));
+        }
+
+        private void AddLast(Node<T> node)
+        {
+            if (IsEmpty)
+                Head = node;
+            else
+                Tail.Next = node;
+
+            Tail = node;
+
+            count++;
+        }
+
+        public void Remove() 
+        {
+            if (IsEmpty)
+                throw  new InvalidOperationException();
+
+            Head = Head.Next;
+            if (count == 1)
+                Tail.Next = null;
+
+            count--;
+
+        }
+            private bool IsEmpty => count == 0;
+       
+    }
+        
+}
+
